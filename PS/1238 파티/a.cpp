1ec081dc661ba3724a753efc6db197d20468b80e
RGH55 <rgh550226@gmail.com>
2025-08-12 21:15:32 +0900

    08/12 3rd

--- PS/1238 파티/a.cpp
+++ PS/1238 파티/a.cpp
@@ -2,11 +2,42 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
+#define INF 100000000
 
 using namespace std;
 
+int N, M, X;
+int dist[2][1005];
+vector<pair<int, int> > vt[2][1005];
+priority_queue<pair<int, int> > pq;
+
 int main()
 {
-
+    scanf("%d %d %d", &N, &M, &X);
+    for(int i=0; i<M; i++){
+        int s, e, t;
+        scanf("%d %d %d", &s, &e, &t);
+        vt[0][s].push_back({e, t});
+        vt[1][e].push_back({s, t});
+    }
+    for(int i=1; i<=N; i++) dist[0][i] = dist[1][i] = INF;
+    dist[0][X] = dist[1][X] = 0;
+    for(int i=0; i<2; i++){
+        pq.push({0, X});
+        while(!pq.empty()){
+            int d = -pq.top().first;
+            int n = pq.top().second;
+            pq.pop();
+            if(dist[i][n] < d) continue;
+            for(vector<pair<int, int> >::iterator it = vt[i][n].begin(); it != vt[i][n].end(); ++it){
+                    if(d + it->second < dist[i][it->first]){
+                        dist[i][it->first] = d + it->second;
+                        pq.push({-dist[i][it->first], it->first});
+                    }
+            }
+        }
+    }
+    for(int i=1; i<=N; i++) dist[0][i] += dist[1][i];
+    printf("%d\n", *max_element(dist[0]+1, dist[0]+N+1));
     return 0;
 }
--- PS/2467 용액/a.cpp
+++ PS/2467 용액/a.cpp
@@ -1,64 +1,27 @@
 #include <cstdio>
 #include <algorithm>
-#include <cmath>
-#include <utility>
 
 using namespace std;
 
 int N;
 int a[100005];
 
-<<<<<<< HEAD
-=======
-pair<int, int> f(int s, int e, int n)
-{
-    if(s >= e) return {abs(n + a[s]), s};
-    int m = (s+e)/2;
-    if(n > a[m]){
-        pair<int, int> p = f(m+1, e, n);
-        if(p.first < abs(n + a[m])) return p;
-        return {abs(n + a[m]), m};
-    }
-    if(n < a[m]){
-        pair<int, int> p = f(s, m, n);
-        if(p.first < abs(n + a[m])) return p;
-        return {abs(n + a[m]), m};
-    }
-    return {2e9, m};
-}
-
->>>>>>> bde26bf3b8c4cc48434e12731ff51cc1d7322f63
 int main()
 {
     scanf("%d", &N);
     for(int i=0; i<N; i++) scanf("%d", &a[i]);
-    sort(a, a+N);
-<<<<<<< HEAD
-    int ans = abs(a[0] + a[1]);
-    pair<int, int> p = {0, 1};
-    for(int i=0; i<N; i++){
-        int s = 0, e = N;
-        while(s < e){
-            int m = (s + e) / 2;
-            if(m != i && abs(a[m] + a[i]) < ans){
-                ans = abs(a[m] + a[i]);
-                p = {min(m, i), max(m, i)};
-            }
-            if(a[m] + a[i] > 0) e = m;
-            else s = m + 1;
+    sort(a, a + N);
+    int sum = abs(a[0] + a[1]);
+    int ans[2] = {a[0], a[1]};
+    int s = 0, e = N - 1;
+    while(s < e){
+        if(abs(a[s] + a[e]) < sum){
+            sum = abs(a[s] + a[e]);
+            ans[0] = a[s]; ans[1] = a[e];
         }
+        if(a[s] + a[e] > 0) e--;
+        else s++;
     }
-    printf("%d %d\n", a[p.first], a[p.second]);
-=======
-    pair<pair<int, int>, int> ans = {f(0, N, a[0]), 0};
-    printf("%d %d\n", ans.first, ans.first.second);
-    for(int i=1; i<N; i++){
-        pair<int, int> p = f(0, N, a[i]);
-        printf("%d %d\n", p.first, p.second);
-        if(p.first < ans.first.first) ans = {p, i};
-    }
-    if(ans.first.first < ans.second) printf("%d %d\n", a[ans.first.first], a[ans.second]);
-    else printf("%d %d\n", a[ans.second], a[ans.first.first]);
->>>>>>> bde26bf3b8c4cc48434e12731ff51cc1d7322f63
+    printf("%d %d\n", ans[0], ans[1]);
     return 0;
 }
